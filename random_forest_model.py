ae0e23cedb3cc185e6640d1f25725ef5a69e7af1
Shashank Dontireddy <shashank.dontireddy@gmail.com>
2026-03-29 16:20:36 -0400

    Update random_forest_model.py
    Add timing of training and testing.
    Change the return to include all evaluation metrics

--- random_forest_model.py
+++ random_forest_model.py
@@ -1,18 +1,28 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score
+import time
 
 def run_random_forest(X_train, X_test, y_train, y_test):
-    # Training the model and setting the number of trees to a 100
-    # Using all CPU cores for fast training
+    # Initialize the model with 100 trees and as many CPU cores as possible
     model = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+    
+    # Train the model and timing how long it takes
+    start_train = time.time()
     model.fit(X_train, y_train)
+    train_time = time.time() - start_train
     
-    # Testing the model and showing the metric of evaluation
+    # Testing the model and timing how long it takes
+    start_test = time.time()
     preds = model.predict(X_test)
-    print("--- Random Forest Metrics ---")
-    print(f"Accuracy:  {accuracy_score(y_test, preds):.4f}")
-    print(f"Precision: {precision_score(y_test, preds):.4f}")
-    print(f"F1 Score:  {f1_score(y_test, preds):.4f}")
+    test_time = time.time() - start_test
     
-    # Returning the predictions for confusion matrix 
-    return preds
+    # Get the metrics of evaluation and time to return to main
+    metrics = {
+        'model': 'Random Forest',
+        'accuracy': accuracy_score(y_test, preds),
+        'f1_score': f1_score(y_test, preds),
+        'train_time': train_time,
+        'test_time': test_time,
+        'preds': preds
+    }
+    return metrics
